ad9794977e973ce973f292af6f887ffe03cc4684
SenorPez <senorpez@senorpez.com>
2016-01-12 23:21:36 -0600

    Fixed broken show_participants script. Now takes config file.

--- show_participants.py
+++ show_participants.py
@@ -4,14 +4,14 @@ import sys
 
 from get_telemetry import get_telemetry
 #import replay_globals as g
-g = import_module(sys.argv[2][:-3])
 
 if len(sys.argv) != 2:
-	print("Usage: 'python'"+sys.argv[0]+" <packetdirectory>'")
+	print("Usage: 'python'"+sys.argv[0]+" <configfile'")
 
 else:
-	get_telemetry(sys.argv[1])
+	g = import_module(sys.argv[1][:-3])
+	get_telemetry(g.sourcetelemetry)
 	#string = "\n".join("{0:>2}: {1}".format(sorted([(int(i), str(n)) for (i, n) in g.participantData])))
-	string = "\n".join(["{0:>2}: {1}".format(x, y) for (x, y) in sorted([(int(i), str(n)) for (i, n) in g.participantData])])
+	string = "\n".join(["{0:>2}: {1}".format(x, y) for (x, y) in sorted([(int(i), str(n)) for i, n, *rest in g.participantData])])
 	#string = "\n".join(natsorted(["{0:>2}: {1}".format(int(i), n) for (i, n) in g.participantData]))
 	print(string)
