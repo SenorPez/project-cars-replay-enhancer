41cc1313e580c8e1b9e011afb39fac5f4897e80a
SenorPez <senorpez@senorpez.com>
2016-01-27 21:14:01 -0600

    Fixes #34. Falls back to luminosity-based scene detection if blackframe detection fails.

--- black_test.py
+++ black_test.py
@@ -3,7 +3,7 @@ from hashfile import hashfile
 from hashlib import sha256
 from importlib import import_module
 import moviepy.editor as mpy
-from numpy import diff, where
+from numpy import diff, where, nonzero
 import os.path
 import sys
 
@@ -31,20 +31,38 @@ def black_test(filename):
 		#Nothing found in the cache, so detect and add to cache.
 		video = mpy.VideoFileClip(filename)
 		
-		try:
-			blackframes = [t for (t, f) in video.iter_frames(with_times=True) if f.mean() < g.threshold]
+		blackframes = [t for (t, f) in video.iter_frames(with_times=True, progress_bar=1) if f.mean() < g.threshold]
 
+		try:
+			videostart = blackframes[where(diff(blackframes)>g.gaptime)[0][0+g.skipstart]]
+		except IndexError:
+			#Some replays don't use blackframes to separate frames. In this case
+			#the only blackframes are the start and stop of the replay.
 			videostart = blackframes[where(diff(blackframes)>g.gaptime)[0][0]]
+			luminosity_difference = abs(diff([f.sum() for f in video.subclip(videostart,videostart+60).iter_frames(dtype=float, progress_bar=1)]))
+			luminosity_average = luminosity_difference.mean()
+			luminosity_jumps = 1+nonzero(luminosity_difference>10*luminosity_average)[0]
+			tt = [videostart] + list((1.0/video.fps)*luminosity_jumps+videostart) + [videostart+60]
+			try:
+				videostart = [tt[x] for x in nonzero(diff(tt)>1)[0]][0+g.skipstart]
+			except IndexError:
+				videostart = 0
+
+		try:
 			videoend = blackframes[::-1][where(diff(blackframes[::-1])<-g.gaptime)[0][0+g.skipend]]
 		except IndexError:
 			#Some replays don't use blackframes to separate frames. In this case
 			#the only blackframes are the start and stop of the replay.
-			if len(where(diff(blackframes)>g.gaptime)[0]):
-				videostart = blackframes[where(diff(blackframes)>g.gaptime)[0][0]]
-				videoend = blackframes[::-1][where(diff(blackframes[::-1])<-g.gaptime)[0][0+g.skipend]]
-			else:
-				videostart = 0
+			videoend = blackframes[::-1][where(diff(blackframes[::-1])<-g.gaptime)[0][0]]
+			luminosity_difference = abs(diff([f.sum() for f in video.subclip(videoend-60,videoend).iter_frames(dtype=float, progress_bar=1)]))
+			luminosity_average = luminosity_difference.mean()
+			luminosity_jumps = 1+nonzero(luminosity_difference>10*luminosity_average)[0]
+			tt = [videoend-60] + list((1.0/video.fps)*luminosity_jumps+videoend-60) + [videoend]
+			try:
+				videoend = [tt[x] for x in nonzero(diff(tt)>1)[0]][0-g.skipend]
+			except IndexError:
 				videoend = video.duration
+
 		finally:
 			with open(g.cachefile, 'a') as h:
 				cache = csv.writer(h)
