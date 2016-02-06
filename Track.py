92d521fe93c5b5464c5d033be7e28474826fb08c
SenorPez <senorpez@senorpez.com>
2016-02-05 19:49:19 -0600

    Added support for pitless tracks. Added more tracks to data file.

--- Track.py
+++ Track.py
@@ -21,37 +21,45 @@ class Track():
     - Pit Detection Radius: float
     """
     def __init__(self, track_length, reverse=False):
-        try:
-            with open(os.path.realpath("track_data.json"), 'r') \
-                    as json_file:
-                try:
-                    json_data = json.load(json_file)
-                except ValueError as json_error:
-                    raise json_error
+        with open(os.path.realpath("track_data.json"), 'r') \
+                as json_file:
+            try:
+                json_data = json.load(json_file)
+            except ValueError as json_error:
+                raise json_error
 
-            integer, _, decimal = track_length.partition('.')
-            track_length = ".".join([integer, (decimal+'0'*3)[:3]])
-            track = [y for x in json_data['tracks'] \
-                for y in x.values() \
-                if y['length'] == float(track_length)]
+        integer, _, decimal = str(track_length).partition('.')
+        track_length = ".".join([integer, (decimal+'0'*3)[:3]])
+        track = [y for x in json_data['tracks'] \
+            for y in x.values() \
+            if y['length'] == float(track_length)]
 
-            if len(track) != 1:
-                raise ValueError("ValueError: Duplicate or incorrect \
-                    length found in data. Correct before proceeding.")
-            else:
-                track = track[0]
-                self.name = str(track['display_name'])
-                if reverse:
-                    self.name += " Reverse"
-                self.length = float(track['length'])
+        if len(track) == 0:
+            raise ValueError("ValueError: No matching track found \
+                in data.")
+        if len(track) > 1:
+            raise ValueError("ValueError: Multiple matching tracks \
+                found in data.")
+        else:
+            track = track[0]
+            self.name = str(track['display_name'])
+            if reverse:
+                self.name += " Reverse"
+            self.length = float(track['length'])
+
+            try:
+                #If all of the pit keys are present, we populate the
+                #pit information.
                 self.pit = True
                 self.pit_entry = [float(track['pit_entry'][0]),
                                   float(track['pit_entry'][1])]
                 self.pit_exit = [float(track['pit_exit'][0]),
                                  float(track['pit_exit'][1])]
                 self.pit_radius = float(track['pit_radius'])
-        except ValueError as track_data_error:
-            print(track_data_error)
+            except KeyError:
+                #If any of the pit keys aren't present, we treat it
+                #like there's no pit on the track.
+                self.pit = False
 
     def at_pit_entry(self, coordinates):
         """
