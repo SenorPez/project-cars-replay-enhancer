df8f416de4b3bff08c820c23e181ccd6a15a2400
SenorPez <senorpez@senorpez.com>
2016-02-22 16:00:49 -0600

    Garbage cleanup.

--- ReplayEnhancer.py
+++ ReplayEnhancer.py
@@ -67,10 +67,6 @@ class ReplayEnhancer():
         self.team_data = {k:v['team'] for k, v in json_data['participant_config'].items()}
         self.points = {k:v['points'] for k, v in json_data['participant_config'].items()}
 
-        #self.car_data = {v['participant']:v['car'] for v in json_data['participant_config']}
-        #self.team_data = {v['participant']:v['team'] if len(v['team']) else None for v in json_data['participant_config']}
-        #self.points = {v['participant']:v['points'] for v in json_data['participant_config']}
-
         self.point_structure = json_data['point_structure']
 
         self.video_threshold = json_data['video_threshold']
@@ -86,14 +82,6 @@ class ReplayEnhancer():
         self.participant_lookup = list()
         self.telemetry_data = list()
         self.config_version = 4
-
-        '''
-        self.sector_bests = [-1, -1, -1]
-        self.personal_bests = [[-1, -1, -1] for x in range(64)]
-        self.best_lap = -1
-        self.personal_best_laps = [-1 for x in range(64)]
-        self.elapsed_times = [-1 for x in range(64)]
-        '''
 
         self.race_start = -1
         self.race_finish = -1
@@ -126,23 +114,6 @@ class ReplayEnhancer():
                     self.telemetry_data.append(row+[i])
                     i += 1
                     progress_bar.update()
-
-                    '''
-                    if int(row[1]) & 3 == 0:
-                        self.telemetry_data.append(row)
-                    elif int(row[1]) & 3 == 1:
-                        for p in enumerate(row[6:-1]):
-                            if len(p[1]):
-                                self.participant_data.append(p)
-                    elif int(row[1]) & 3 == 2:
-                        for p in enumerate(row[3:-1], int(row[2])):
-                            if len(p[1]):
-                                self.participant_data.append(p)
-                    else:
-                        raise ValueError("ValueError: Unrecognized packet type ("+str(int(row[1]) & 3)+")")
-
-                self.participant_data = [(i, n, t, c) for (i, n), t, c in zip(list(sorted({x for x in self.participant_data})), self.team_data, self.car_data)]
-                '''
 
             #Extract, process, and de-garbage the participant data.
             #Also add cumulative time index to end of data structure.
@@ -274,26 +245,6 @@ class ReplayEnhancer():
                     if self.telemetry_data[tg_i][0][tele_i][-1] != -1:
                         self.telemetry_data[tg_i][0][tele_i][-1] -= time_adjust
 
-            '''
-            data = self.telemetry_data[0][0]
-            self.starting_grid = sorted(((str(int(data[182+i*9]) & int('01111111', 2)), n, t if t is not None else "", c) for i, n, t, c in self.participant_data), key=lambda x: int(x[0]))
-
-            data = self.telemetry_data[self.race_finish]
-            finishers = list()
-            for i, n in self.participant_configurations[-1][:-1]:
-                name = self.participant_lookup[n]
-                finishers.append((i, name, self.team_data[name], self.car_data[name]))
-
-            lead_lap_indexes = [i for i, *rest in finishers if int(data[184+i*9]) >= int(data[10])]
-            lapped_indexes = [i for i, *rest in finishers if int(data[184+i*9]) < int(data[10])]
-
-            data = self.telemetry_data[-1]
-            self.classification = sorted((int(data[182+i*9]) & int('01111111', 2), n, t if t is not None else "", c, i, int(data[10])) for i, n, t, c in finishers if i in lead_lap_indexes)
-            self.classification += sorted((int(data[182+i*9]) & int('01111111', 2), n, t if t is not None else "", c, i, int(data[183+i*9])) for i, n, t, c in finishers if i in lapped_indexes)
-            self.classification = [(p,) + tuple(rest) for p, (i, *rest) in enumerate(self.classification, 1)]
-            import pdb; pdb.set_trace()
-            '''
-
         except ValueError as e:
             print("{}".format(e), file=sys.stderr)
         finally:
@@ -305,13 +256,6 @@ class ReplayEnhancer():
             #Find the name in the lookup, to use the de-junked version of the name.
             name = self.participant_lookup[n]
             participant_data.append((i, name, self.team_data[name], self.car_data[name]))
-            '''
-            for clean__name, raw_names in self.participant_lookup.items():
-                for raw_name in raw_names:
-                    if name[1] == raw_name:
-                        participant_data.append(name)
-            '''
-        #participant_data = [(i, n, t, c) for (i, n), t, c in zip(list(sorted({x for x in participant_data})), self.team_data, self.car_data)]
         return participant_data
 
     def process_telemetry(self):
@@ -691,13 +635,6 @@ class ReplayEnhancer():
         outro = mpy.CompositeVideoClip(outro_videos).set_duration(
             sum([x.duration for x in outro_videos[1:]])).fx(vfx.fadein, 1)
 
-        '''
-        if self.show_champion:
-            outro = mpy.CompositeVideoClip([backdrop, result, series_standings, champion]).set_duration(sum([x.duration for x in [result, series_standings, champion]])).fx(vfx.fadein, 1)
-        else:
-            outro = mpy.CompositeVideoClip([backdrop, result, series_standings]).set_duration(sum([x.duration for x in [result, series_standings]])).fx(vfx.fadein, 1)
-        '''
-
         output = mpy.concatenate_videoclips([intro, mainevent, outro])
         return output
 
@@ -762,101 +699,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Aborting Project CARS Replay Enhancer.")
         raise
-    '''
-    if len(sys.argv) == 1:
-        print("No configuration file provided.")
-        print("Do you want to create a new configuration file?")
-
-        while True:
-            create_config = input("[Y/n]-->> ")
-            if len(create_config) == 0 or \
-                    str.lower(create_config) == "y":
-                config = Configuration()
-                break;
-            elif str.lower(create_config) == "n":
-                print ("Usage: python ReplayEnhancer.py",
-                    "<configuration_file")
-                break
-
-
-
-    try:
-        replay = ReplayEnhancer(sys.argv[1])
-    except ValueError as e:
-        print("Invalid JSON in configuration file: {}".format(e))
-    else:
-        if replay.source_video is None:
-            video = mpy.ColorClip((1280, 720), duration=replay.telemetry_data[-1][0][-1][-1])
-        else:
-            video = replay.black_test()
-        video_width, video_height = video.size
-
-        if replay.backdrop != "":
-            backdrop = Image.open(replay.backdrop).resize((video_width, video_height))
-            if replay.logo != "":
-                logo = Image.open(replay.logo).resize((replay.logo_width, replay.logo_height))
-                backdrop.paste(logo, (backdrop.width-logo.width, backdrop.height-logo.height), logo)
-        else:
-            backdrop = Image.new('RGBA', (video_width, video_height), (0, 0, 0))
-            if replay.logo != "":
-                logo = Image.open(replay.logo).resize((replay.logo_width, replay.logo_height))
-                backdrop.paste(logo, (backdrop.width-logo.width, backdrop.height-logo.height), logo)
-
-        backdrop = mpy.ImageClip(PIL_to_npimage(backdrop))
-        title = mpy.ImageClip(Title(replay).to_frame()).set_duration(6).set_position(('center', 'center'))
-
-        standing = UpdatedVideoClip(Standings(replay, process_data=True))
-        standing = standing.set_position((replay.margin, replay.margin)).set_duration(video.duration)
-        standing_mask = mpy.ImageClip(Standings(replay).make_mask(), ismask=True, duration=video.duration)
-        standing = standing.set_mask(standing_mask)
-
-        timer = UpdatedVideoClip(Timer(replay, process_data=True))
-        timer_width, timer_height = timer.size
-        timer = timer.set_position((video_width-timer_width-replay.margin, replay.margin)).set_duration(video.duration)
-        timer_mask = mpy.ImageClip(Timer(replay).make_mask(), ismask=True, duration=video.duration)
-        timer = timer.set_mask(timer_mask)
-
-        result = mpy.ImageClip(Results(replay).to_frame()).set_duration(20).set_position(('center', 'center')).add_mask()
-        result.mask = result.mask.fx(vfx.fadeout, 1)
-
-        series_standings = mpy.ImageClip(SeriesStandings(replay).to_frame()).set_start(20).set_duration(20).set_position(('center', 'center')).add_mask()
-
-        if replay.show_champion:
-            series_standings.mask = series_standings.mask.fx(vfx.fadein, 1).fx(vfx.fadeout, 1)
-            champion = mpy.ImageClip(Champion(replay).to_frame()).set_start(40).set_duration(20).set_position(('center', 'center')).add_mask()
-            champion.mask = champion.mask.fx(vfx.fadein, 1)
-        else:
-            series_standings.mask = series_standings.mask.fx(vfx.fadein, 1)
-
-        intro = mpy.CompositeVideoClip([backdrop, title]).set_duration(6).fx(vfx.fadeout, 1)
-        mainevent = mpy.CompositeVideoClip([video, standing, timer]).set_duration(video.duration)
-
-        if replay.show_champion:
-            outro = mpy.CompositeVideoClip([backdrop, result, series_standings, champion]).set_duration(sum([x.duration for x in [result, series_standings, champion]])).fx(vfx.fadein, 1)
-        else:
-            outro = mpy.CompositeVideoClip([backdrop, result, series_standings]).set_duration(sum([x.duration for x in [result, series_standings]])).fx(vfx.fadein, 1)
-
-        output = mpy.concatenate_videoclips([intro, mainevent, outro])
-
-        #title.save_frame("outputs/title.jpg", 0)
-        #series_standings.save_frame("outputs/series_standings.jpg", 0)
-
-        #Full video.
-        #output.write_videofile(replay.output_video, fps=30)
-        
-        #Full video, low framerate
-        #output.write_videofile(replay.output_video, fps=10)
-
-        #Subclip video.
-        #output.subclip(1*60+50, 2*60).write_videofile(replay.output_video, fps=10, preset='superfast')
-        output.subclip(0, 8).write_videofile(replay.output_video, fps=10, preset='superfast')
-        #output.subclip(1020, 1260).write_videofile(replay.output_video, fps=10, preset='superfast')
-        #output.subclip(output.duration-80, output.duration).write_videofile(replay.output_video, fps=10)
-
-        #Single frame.
-        #output.save_frame(replay.output_video+".jpg", 400)
-
-        #Multiple frames.
-        #for frame in range(int(output.duration-45), int(output.duration-25)):
-            #output.save_frame(replay.output_video+"."+str(frame)+".jpg", frame)
-    '''
