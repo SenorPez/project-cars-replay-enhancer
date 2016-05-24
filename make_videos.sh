932be25a3d12267a6c38957925fc3aeff724927c
SenorPez <senorpez@senorpez.com>
2016-05-24 15:01:16 -0500

    Modified helper script for new file formats.

--- make_videos.sh
+++ make_videos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for f in $@
 do
-	SNAME=$(sed -r 's/.*(race[[:digit:]]+.*)_.*/\1/g' <<< "$f")
-	screen -dmS $SNAME python3 replay.py $f
+	SNAME=$(sed -r 's/.*(race[[:digit:]]+.*).*\.json/\1/g' <<< "$f")
+	screen -dmS $SNAME python3 ReplayEnhancer.py $f
 done
