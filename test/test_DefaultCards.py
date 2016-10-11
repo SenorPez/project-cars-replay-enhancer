3e47048bf517a10e8d4d7cc487ab0b718de98d47
SenorPez <senorpez@senorpez.com>
2016-10-11 15:32:07 -0500

    Added tests for SeriesStandings card.

--- test/test_DefaultCards.py
+++ test/test_DefaultCards.py
@@ -160,3 +160,165 @@ class TestStartingGrid(unittest.TestCase):
         instance = StartingGrid([mock_starting_grid_entry], **configuration)
         expected_result = StartingGrid
         self.assertIsInstance(instance, expected_result)
+
+
+class TestSeriesStandings(unittest.TestCase):
+    """
+    Unit Tests for Series Standings card.
+    """
+
+    @patch('replayenhancer.StaticBase.StaticBase.sort_data', autospec=True)
+    @patch('replayenhancer.RaceData.ClassificationEntry', autospec=True)
+    def test_init_no_config(self, mock_classification_entry, mock_sort_data):
+        mock_sort_data.return_value = ('Kobernulf Monnur', 1, 42.0)
+
+        instance = SeriesStandings(mock_classification_entry)
+        expected_result = SeriesStandings
+        self.assertIsInstance(instance, expected_result)
+
+    @patch('replayenhancer.StaticBase.StaticBase.sort_data', autospec=True)
+    @patch('replayenhancer.RaceData.ClassificationEntry', autospec=True)
+    def test_init_config(self, mock_classification_entry, mock_sort_data):
+        mock_sort_data.return_value = ('Kobernulf Monnur', 1, 42.0)
+
+        configuration = {
+            'participant_config': {
+                'Kobernulf Monnur': {
+                    'display': 'Senor Pez',
+                    'car': '125cc Shifter Kart',
+                    'team': 'Dark Nitro',
+                    'points': 15
+                }
+            },
+            'point_structure': [5, 15, 12, 10, 8, 6, 4, 2, 1]
+        }
+
+        instance = SeriesStandings([mock_classification_entry], **configuration)
+        expected_result = SeriesStandings
+        self.assertIsInstance(instance, expected_result)
+
+    @patch('replayenhancer.StaticBase.StaticBase.sort_data', autospec=True)
+    @patch('replayenhancer.RaceData.ClassificationEntry', autospec=True)
+    def test_method_calc_series_points(self, mock_classification_entry, mock_sort_data):
+        driver_name = 'Kobernulf Monnur'
+        position = 1
+        best_lap = 42.0
+
+        mock_sort_data.return_value = (driver_name, position, best_lap)
+        type(mock_classification_entry).best_lap = PropertyMock(
+            return_value=best_lap)
+
+        configuration = {
+            'points_lookup': {
+                'Kobernulf Monnur': 15
+            },
+            'point_structure': [5, 15, 12, 10, 8, 6, 4, 2, 1]
+        }
+
+        instance = SeriesStandings([mock_classification_entry])
+        expected_result = '35'
+        self.assertEqual(
+            instance.calc_series_points(
+                (driver_name, position, best_lap),
+                **configuration),
+            expected_result)
+
+    @patch('replayenhancer.StaticBase.StaticBase.sort_data', autospec=True)
+    @patch('replayenhancer.RaceData.ClassificationEntry', autospec=True)
+    def test_method_calc_series_points_no_entry(self, mock_classification_entry, mock_sort_data):
+        driver_name = 'Kobernulf Monnur'
+        position = 1
+        best_lap = 42.0
+
+        mock_sort_data.return_value = (driver_name, position, best_lap)
+        type(mock_classification_entry).best_lap = PropertyMock(
+            return_value=best_lap)
+
+        configuration = {
+            'point_structure': [5, 15, 12, 10, 8, 6, 4, 2, 1]
+        }
+
+        instance = SeriesStandings([mock_classification_entry])
+        expected_result = '20'
+        self.assertEqual(
+            instance.calc_series_points(
+                (driver_name, position, best_lap),
+                **configuration),
+            expected_result)
+
+    @patch('replayenhancer.StaticBase.StaticBase.sort_data', autospec=True)
+    @patch('replayenhancer.RaceData.ClassificationEntry', autospec=True)
+    def test_method_calc_series_rank(self, mock_classification_entry, mock_sort_data):
+        driver_name = 'Kobernulf Monnur'
+        position = 1
+        best_lap = 42.0
+
+        mock_sort_data.return_value = (driver_name, position, best_lap)
+        type(mock_classification_entry).best_lap = PropertyMock(
+            return_value=best_lap)
+        type(mock_classification_entry).calc_points_data = PropertyMock(
+            return_value=(driver_name, position, best_lap))
+        type(mock_classification_entry).driver_name = PropertyMock(
+            return_value=driver_name)
+
+        configuration = {
+            'points_lookup': {
+                'Kobernulf Monnur': 15
+            },
+            'point_structure': [5, 15, 12, 10, 8, 6, 4, 2, 1]
+        }
+
+        instance = SeriesStandings([mock_classification_entry])
+        expected_result = '1'
+        self.assertEqual(
+            instance.calc_series_rank(
+                (driver_name, position, best_lap),
+                **configuration),
+            expected_result)
+
+    @patch('replayenhancer.StaticBase.StaticBase.sort_data', autospec=True)
+    @patch('replayenhancer.RaceData.ClassificationEntry')
+    def test_method_calc_series_rank_2(self, mock_classification_entry, mock_sort_data):
+        mock_sort_data.return_value = [
+            ('First Place', 1, 42.0),
+            ('Second Place', 2, 42.0),
+            ('Third Place', 3, 42.0)
+        ]
+
+        patcher = patch(
+            'replayenhancer.RaceData.ClassificationEntry',
+            best_lap=42.0,
+            calc_points_data=('First Place', 1, 42.0),
+            driver_name='First Place')
+        first_place = patcher.start()
+
+        patcher = patch(
+            'replayenhancer.RaceData.ClassificationEntry',
+            best_lap=42.0,
+            calc_points_data=('Second Place', 2, 42.0),
+            driver_name='Second Place')
+        second_place = patcher.start()
+
+        patcher = patch(
+            'replayenhancer.RaceData.ClassificationEntry',
+            best_lap=42.0,
+            calc_points_data=('Third Place', 3, 42.0),
+            driver_name='Third Place')
+        third_place = patcher.start()
+
+        configuration = {
+            'points_lookup': {
+                'First Place': 10,
+                'Second Place': 10,
+                'Third Place': 5
+            },
+            'point_structure': [0, 10, 10, 6]
+        }
+
+        instance = SeriesStandings([first_place, second_place, third_place])
+        expected_result = '2'
+        self.assertEqual(
+            instance.calc_series_rank(
+                ('Third Place', 3, 42.0),
+                **configuration),
+            expected_result)
