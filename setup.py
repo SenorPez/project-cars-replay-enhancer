b20291de7e79d336163eb1d938d52605b18fb6ca
SenorPez <senorpez@senorpez.com>
2016-12-01 10:05:44 -0600

    Updated version to RC5 for release.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='replayenhancer',
-    version='0.5rc4',
+    version='0.5rc5',
     description='Replay Enhancer',
     long_description=long_description,
     url='https://github.com/SenorPez/project-cars-replay-enhancer',
