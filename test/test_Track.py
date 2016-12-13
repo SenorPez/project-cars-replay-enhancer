38173e5eb1dddbe351713e204abd21381d531397
SenorPez <senorpez@senorpez.com>
2016-12-12 22:29:49 -0600

    Updating test_Track.py for pre-3.5 support.

--- test/test_Track.py
+++ test/test_Track.py
@@ -2,10 +2,10 @@
 
 """
 
-
 import unittest
 from unittest.mock import mock_open, patch
 
+import sys
 from replayenhancer.Track import Track
 
 
@@ -28,8 +28,18 @@ class TestTrack(unittest.TestCase):
         }
     }
 
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_init(self):
         with patch('replayenhancer.Track.open', mock_open()), \
+                patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            expected_result = Track
+            self.assertIsInstance(instance, expected_result)
+
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_init_pre35(self):
+        with patch('builtins.open', mock_open()), \
                 patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
@@ -55,121 +65,121 @@ class TestTrack(unittest.TestCase):
                 patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertTrue(instance.at_pit_entry((0, 0)))
+            self.assertTrue(instance.at_pit_entry([0, -99, 0]))
 
     def test_at_pit_entry_close_x_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertTrue(instance.at_pit_entry((1, 0)))
+            self.assertTrue(instance.at_pit_entry([1, -99, 0]))
 
     def test_at_pit_entry_edge_x_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertFalse(instance.at_pit_entry((2, 0)))
+            self.assertFalse(instance.at_pit_entry([2, -99, 0]))
 
     def test_at_pit_entry_far_x_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertFalse(instance.at_pit_entry((3, 0)))
+            self.assertFalse(instance.at_pit_entry([3, -99, 0]))
 
     def test_at_pit_entry_close_y_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertTrue(instance.at_pit_entry((0, 1)))
+            self.assertTrue(instance.at_pit_entry([0, -99, 1]))
 
     def test_at_pit_entry_exact_y_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertFalse(instance.at_pit_entry((0, 2)))
+            self.assertFalse(instance.at_pit_entry([0, -99, 2]))
 
     def test_at_pit_entry_far_y_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertFalse(instance.at_pit_entry((0, 3)))
+            self.assertFalse(instance.at_pit_entry([0, -99, 3]))
 
     def test_at_pit_entry_no_pits(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(15.0)
-            self.assertFalse(instance.at_pit_entry((0, 0)))
+            self.assertFalse(instance.at_pit_entry([0,-99, 0]))
 
     def test_at_pit_entry_no_json(self):
         with patch('replayenhancer.Track.open', mock_open()) as mock_open_error:
             mock_open_error.side_effect = FileNotFoundError
             instance = Track(42.0)
-            self.assertFalse(instance.at_pit_entry((0, 0)))
+            self.assertFalse(instance.at_pit_entry([0, -99, 0]))
 
     def test_at_pit_exit_exact_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertTrue(instance.at_pit_exit((100, 100)))
+            self.assertTrue(instance.at_pit_exit([100, -99, 100]))
 
     def test_at_pit_exit_close_x_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertTrue(instance.at_pit_exit((101, 100)))
+            self.assertTrue(instance.at_pit_exit([101, -99, 100]))
 
     def test_at_pit_exit_edge_x_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertFalse(instance.at_pit_exit((102, 100)))
+            self.assertFalse(instance.at_pit_exit([102, -99, 100]))
 
     def test_at_pit_exit_far_x_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertFalse(instance.at_pit_exit((103, 100)))
+            self.assertFalse(instance.at_pit_exit([103, -99, 100]))
 
     def test_at_pit_exit_close_y_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertTrue(instance.at_pit_exit((100, 101)))
+            self.assertTrue(instance.at_pit_exit([100, -99, 101]))
 
     def test_at_pit_exit_edge_y_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertFalse(instance.at_pit_exit((100, 102)))
+            self.assertFalse(instance.at_pit_exit([100, -99, 102]))
 
     def test_at_pit_exit_far_y_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
-            self.assertFalse(instance.at_pit_exit((100, 103)))
+            self.assertFalse(instance.at_pit_exit([100, -99, 103]))
 
     def test_at_pit_exit_no_pits(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(15.0)
-            self.assertFalse(instance.at_pit_exit((100, 100)))
+            self.assertFalse(instance.at_pit_exit([100, -99, 100]))
 
     def test_at_pit_exit_no_json(self):
         with patch('replayenhancer.Track.open', mock_open()) as mock_open_error:
             mock_open_error.side_effect = FileNotFoundError
             instance = Track(42.0)
-            self.assertFalse(instance.at_pit_exit((0, 0)))
+            self.assertFalse(instance.at_pit_exit([0, -99, 0]))
