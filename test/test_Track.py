8299348d147baaf58551cc3abf151007e7a52715
SenorPez <senorpez@senorpez.com>
2016-12-12 22:42:49 -0600

    Updated remainder of test_Track.py for pre-3.5 support.

--- test/test_Track.py
+++ test/test_Track.py
@@ -2,10 +2,10 @@
 
 """
 
+import sys
 import unittest
 from unittest.mock import mock_open, patch
 
-import sys
 from replayenhancer.Track import Track
 
 
@@ -46,6 +46,7 @@ class TestTrack(unittest.TestCase):
             expected_result = Track
             self.assertIsInstance(instance, expected_result)
 
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_init_no_json(self):
         with patch('replayenhancer.Track.open', mock_open()) as mock_open_error:
             mock_open_error.side_effect = FileNotFoundError
@@ -53,6 +54,15 @@ class TestTrack(unittest.TestCase):
             expected_result = Track
             self.assertIsInstance(instance, expected_result)
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_init_no_json_pre35(self):
+        with patch('builtins.open', mock_open()) as mock_open_error:
+            mock_open_error.side_effect = FileNotFoundError
+            instance = Track(42.0)
+            expected_result = Track
+            self.assertIsInstance(instance, expected_result)
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_init_bad_json(self):
         with patch('replayenhancer.Track.open', mock_open()), \
                 patch('replayenhancer.Track.load') as mock_json_load:
@@ -60,6 +70,15 @@ class TestTrack(unittest.TestCase):
             with self.assertRaises(ValueError):
                 _ = Track(42.0)
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_init_bad_json_pre35(self):
+        with patch('builtins.open', mock_open()), \
+                patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.side_effect = ValueError
+            with self.assertRaises(ValueError):
+                _ = Track(42.0)
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_entry_exact_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
                 patch('replayenhancer.Track.load') as mock_json_load:
@@ -67,6 +86,15 @@ class TestTrack(unittest.TestCase):
             instance = Track(42.0)
             self.assertTrue(instance.at_pit_entry([0, -99, 0]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_entry_exact_true_pre35(self):
+        with patch('builtins.open', mock_open()), \
+                patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertTrue(instance.at_pit_entry([0, -99, 0]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_entry_close_x_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
@@ -74,6 +102,15 @@ class TestTrack(unittest.TestCase):
             instance = Track(42.0)
             self.assertTrue(instance.at_pit_entry([1, -99, 0]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_entry_close_x_true_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertTrue(instance.at_pit_entry([1, -99, 0]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_entry_edge_x_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
@@ -81,6 +118,15 @@ class TestTrack(unittest.TestCase):
             instance = Track(42.0)
             self.assertFalse(instance.at_pit_entry([2, -99, 0]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_entry_edge_x_false_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertFalse(instance.at_pit_entry([2, -99, 0]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_entry_far_x_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
@@ -88,40 +134,93 @@ class TestTrack(unittest.TestCase):
             instance = Track(42.0)
             self.assertFalse(instance.at_pit_entry([3, -99, 0]))
 
-    def test_at_pit_entry_close_y_true(self):
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_entry_far_x_false_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertFalse(instance.at_pit_entry([3, -99, 0]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
+    def test_at_pit_entry_close_z_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
             self.assertTrue(instance.at_pit_entry([0, -99, 1]))
 
-    def test_at_pit_entry_exact_y_false(self):
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_entry_close_z_true_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertTrue(instance.at_pit_entry([0, -99, 1]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
+    def test_at_pit_entry_exact_z_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
             self.assertFalse(instance.at_pit_entry([0, -99, 2]))
 
-    def test_at_pit_entry_far_y_false(self):
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_entry_exact_z_false_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertFalse(instance.at_pit_entry([0, -99, 2]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
+    def test_at_pit_entry_far_z_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
             self.assertFalse(instance.at_pit_entry([0, -99, 3]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_entry_far_z_false_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertFalse(instance.at_pit_entry([0, -99, 3]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_entry_no_pits(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(15.0)
-            self.assertFalse(instance.at_pit_entry([0,-99, 0]))
+            self.assertFalse(instance.at_pit_entry([0, -99, 0]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_entry_no_pits_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(15.0)
+            self.assertFalse(instance.at_pit_entry([0, -99, 0]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_entry_no_json(self):
         with patch('replayenhancer.Track.open', mock_open()) as mock_open_error:
             mock_open_error.side_effect = FileNotFoundError
             instance = Track(42.0)
             self.assertFalse(instance.at_pit_entry([0, -99, 0]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_entry_no_json_pre35(self):
+        with patch('builtins.open', mock_open()) as mock_open_error:
+            mock_open_error.side_effect = FileNotFoundError
+            instance = Track(42.0)
+            self.assertFalse(instance.at_pit_entry([0, -99, 0]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_exit_exact_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
@@ -129,6 +228,15 @@ class TestTrack(unittest.TestCase):
             instance = Track(42.0)
             self.assertTrue(instance.at_pit_exit([100, -99, 100]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_exit_exact_true_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertTrue(instance.at_pit_exit([100, -99, 100]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_exit_close_x_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
@@ -136,6 +244,15 @@ class TestTrack(unittest.TestCase):
             instance = Track(42.0)
             self.assertTrue(instance.at_pit_exit([101, -99, 100]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_exit_close_x_true_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertTrue(instance.at_pit_exit([101, -99, 100]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_exit_edge_x_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
@@ -143,6 +260,15 @@ class TestTrack(unittest.TestCase):
             instance = Track(42.0)
             self.assertFalse(instance.at_pit_exit([102, -99, 100]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_exit_edge_x_false_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertFalse(instance.at_pit_exit([102, -99, 100]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_exit_far_x_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
@@ -150,27 +276,63 @@ class TestTrack(unittest.TestCase):
             instance = Track(42.0)
             self.assertFalse(instance.at_pit_exit([103, -99, 100]))
 
-    def test_at_pit_exit_close_y_true(self):
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_exit_far_x_false_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertFalse(instance.at_pit_exit([103, -99, 100]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
+    def test_at_pit_exit_close_z_true(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
             self.assertTrue(instance.at_pit_exit([100, -99, 101]))
 
-    def test_at_pit_exit_edge_y_false(self):
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_exit_close_z_true_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertTrue(instance.at_pit_exit([100, -99, 101]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
+    def test_at_pit_exit_edge_z_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
             self.assertFalse(instance.at_pit_exit([100, -99, 102]))
 
-    def test_at_pit_exit_far_y_false(self):
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_exit_edge_z_false_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertFalse(instance.at_pit_exit([100, -99, 102]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
+    def test_at_pit_exit_far_z_false(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
             mock_json_load.return_value = self.track_data
             instance = Track(42.0)
             self.assertFalse(instance.at_pit_exit([100, -99, 103]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_exit_far_z_false_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(42.0)
+            self.assertFalse(instance.at_pit_exit([100, -99, 103]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_exit_no_pits(self):
         with patch('replayenhancer.Track.open', mock_open()), \
              patch('replayenhancer.Track.load') as mock_json_load:
@@ -178,8 +340,24 @@ class TestTrack(unittest.TestCase):
             instance = Track(15.0)
             self.assertFalse(instance.at_pit_exit([100, -99, 100]))
 
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_exit_no_pits_pre35(self):
+        with patch('builtins.open', mock_open()), \
+             patch('replayenhancer.Track.load') as mock_json_load:
+            mock_json_load.return_value = self.track_data
+            instance = Track(15.0)
+            self.assertFalse(instance.at_pit_exit([100, -99, 100]))
+
+    @unittest.skipIf(sys.version_info < (3, 5), "Not supported.")
     def test_at_pit_exit_no_json(self):
         with patch('replayenhancer.Track.open', mock_open()) as mock_open_error:
+            mock_open_error.side_effect = FileNotFoundError
+            instance = Track(42.0)
+            self.assertFalse(instance.at_pit_exit([0, -99, 0]))
+
+    @unittest.skipIf(sys.version_info >= (3, 5), "Not supported.")
+    def test_at_pit_exit_no_json_pre35(self):
+        with patch('builtins.open', mock_open()) as mock_open_error:
             mock_open_error.side_effect = FileNotFoundError
             instance = Track(42.0)
             self.assertFalse(instance.at_pit_exit([0, -99, 0]))
