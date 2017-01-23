6bb2fde20a4d8b8d5955b8aceb819dbaa9168d82
SenorPez <senorpez@senorpez.com>
2017-01-23 15:08:31 -0600

    Updating versions for 0.6rc1

--- replayenhancer/replayenhancer.py
+++ replayenhancer/replayenhancer.py
@@ -251,7 +251,7 @@ def main():
         '-v',
         '--version',
         action='version',
-        version='Version 0.5.2')
+        version='Version 0.6 Release Candidate 1')
 
     args = parser.parse_args()
 
